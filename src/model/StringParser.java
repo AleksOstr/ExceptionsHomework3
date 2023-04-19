66420ba056c4ab0d6f236bff28f6ae12519c02b4
AleksOstr <ostryankoam@yandex.ru>
2023-04-19 19:53:56 +0400

    StringParser done

--- src/model/StringParser.java
+++ src/model/StringParser.java
@@ -1,5 +1,16 @@
 package model;
 
+import model.exceptions.*;
+
 public class StringParser {
+    private final InputValidator inputValidator = new InputValidator();
+
+    public String[] parseString(String inputString) throws InvalidCharactersException,
+            WrongDateException, InvalidGenderException, ExtraDataException, NotEnoughDataException,
+            IllegalArgumentException {
+        if (inputValidator.validate(inputString)) {
+            return inputString.split(" ");
+        } else throw new IllegalArgumentException("Invalid data was entered");
+    }
 
 }
