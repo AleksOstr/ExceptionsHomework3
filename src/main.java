09a44f39f760c16b027c125a882224ff6dcc4a96
AleksOstr <ostryankoam@yandex.ru>
2023-04-19 22:33:26 +0400

    add main

--- src/main.java
+++ src/main.java
@@ -1,2 +1,14 @@
+import controller.Controller;
+import model.DataSaver;
+import model.StringParser;
+import view.View;
+
 public class main {
+    public static void main(String[] args) {
+        StringParser stringParser = new StringParser();
+        DataSaver dataSaver = new DataSaver();
+        Controller controller = new Controller(stringParser, dataSaver);
+        View view = new View(controller);
+        view.run();
+    }
 }
