0519f4579dee100587ea0b551ca7964afb226bb3
AleksOstr <ostryankoam@yandex.ru>
2023-04-19 19:46:15 +0400

    InputValidator done

--- src/model/InputValidator.java
+++ src/model/InputValidator.java
@@ -8,8 +8,31 @@ import java.util.Date;
 
 
 public class InputValidator {
-    public boolean validate(String str) {
-        // FIXME: 19.04.2023
+    private final String nameFormat = "[a-z]+";
+    private final String phoneFormat = "7\\d{10}";
+    private final String genderMale = "m";
+    private final String genderFemale = "f";
+    private final String dateForomat = "dd.MM.yyyy";
+
+    public boolean validate(String str)
+            throws NotEnoughDataException, ExtraDataException, InvalidCharactersException, WrongDateException,
+            InvalidGenderException{
+        String[] strArray = str.split(" ");
+        validateDataCount(strArray);
+        if (!validateName(strArray[0]) || validateName(strArray[1]) || !validateName(strArray[2])) {
+            throw new InvalidCharactersException("Invalid characters were entered in the name");
+        }
+        if (!validateDate(strArray[3])) {
+            throw new WrongDateException("Wrong date was entered");
+        }
+        if (!validateGender(strArray[4])) {
+            throw new InvalidGenderException("Invalid gender was entered");
+        }
+        if (!validatePhoneNumber(strArray[5])) {
+
+            throw new InvalidCharactersException("Invalid characters were entered in the phone number");
+        }
+        return true;
     }
 
     private boolean validateDataCount(String[] strArray) throws NotEnoughDataException, ExtraDataException {
@@ -21,15 +44,15 @@ public class InputValidator {
     }
 
     private boolean validateName(String str) {
-        return str.matches("[a-z]+");
+        return str.matches(nameFormat);
     }
 
     private boolean validateGender(String str) {
-        return str.equals("f") || str.equals("m");
+        return str.equals(genderFemale) || str.equals(genderMale);
     }
 
     private boolean validateDate(String strDate) {
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        SimpleDateFormat format = new SimpleDateFormat(dateForomat);
         format.setLenient(false);
         try {
             Date inputDate = format.parse(strDate);
@@ -40,5 +63,9 @@ public class InputValidator {
         } catch (ParseException e) {
             return false;
         }
+    }
+
+    private boolean validatePhoneNumber(String str) {
+        return str.matches(phoneFormat);
     }
 }
